afa0f4c4666b30b71cb8034053e85b072a69b278
sanjay <sanjay11@outlook.com>
2023-02-03 02:06:49 +0530

    Add migrations

---
+++ Courses.API/Migrations/20230202191408_WatchedRelationId.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Courses.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class WatchedRelationId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // migrationBuilder.DropIndex(
+            //     name: "IX_Watched_CourseId",
+            //     table: "Watched");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Watched_CourseId",
+                table: "Watched",
+                column: "CourseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Watched_CourseId",
+                table: "Watched");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Watched_CourseId",
+                table: "Watched",
+                column: "CourseId",
+                unique: true);
+        }
+    }
+}
